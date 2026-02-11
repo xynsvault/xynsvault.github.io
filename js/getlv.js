28c19220da5caec7114958cf9ba2e41017853e92
xynsvault <xboxynfinity@gmail.com>
2026-02-10 20:10:19 -0500

    Update getlv.js

--- js/getlv.js
+++ js/getlv.js
@@ -1,5 +1,5 @@
 DOMParser.retrieveLVL = function (lvl) {
   var downloadLevel = window.open("","_newtab")
-downloadLevel.window.location = atob(aHR0cHM6Ly94eW5zdmF1bHQuZ2l0aHViLmlvL2Rhc2gv)+lvl+".gmd";
+downloadLevel.window.location = atob("aHR0cHM6Ly94eW5zdmF1bHQuZ2l0aHViLmlvL2Rhc2gv")+lvl+".gmd";
 setTimeout(function(){downloadLevel.window.location="https://www.google.com"},1000,false)
 }
