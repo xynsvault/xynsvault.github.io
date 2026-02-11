f88a0905c2f2b79fd879b7f3e5518e55be19dad5
xynsvault <xboxynfinity@gmail.com>
2026-02-10 19:46:03 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,8 +1,9 @@
 DOMParser.okyouregood = function() {
 
 document.body.innerHTML = `
-<div id="levels">
-<a class="level-container" disabled="">
+<h1>Xynfinity's Vault</h1>
+<ul style="list-style:none;display: flex;">
+<li class="level-container" disabled="">
 <h2 style="font-size:30px;">KOCMOC UNLEASHED</h2>
 <embed style="width:425px;height:240px;" src="https://www.youtube.com/embed/mL0y4HWpPlU"><p></p><div style="background:#3d3d3d;padding:5px;border-radius:8px;width:550px;"><h3>LEVEL INFO:</h3><p style="user-select:none;opacity:0;"></p><h>Level I've been looking for day in and day out. Finally.</h><p style="user-select:none;opacity:0;"></p><pre style="position:relative;width:150px;text-align:left!important;">Creator: CherryTeam
 Type: FULL LEVEL
@@ -12,7 +13,17 @@ Decoration Status: Up To Date
 <div class="button-container">
 <input id="apiX" class="input-dis" placeholder="XV Key">
 <button class="button-dis" id="apiBtn" disabled="">Download</button></div>
-</a>
+</li><a style="opacity:0;user-select:none;">seperate</a>
+<li class="level-container" disabled="">
+<h2 style="font-size:30px;">KOCMOC UNLEASHED</h2>
+<embed style="width:425px;height:240px;" src="https://www.youtube.com/embed/mL0y4HWpPlU"><p></p><div style="background:#3d3d3d;padding:5px;border-radius:8px;width:550px;"><h3>LEVEL INFO:</h3><p style="user-select:none;opacity:0;"></p><h>Level I've been looking for day in and day out. Finally.</h><p style="user-select:none;opacity:0;"></p><pre style="position:relative;width:150px;text-align:left!important;">Creator: CherryTeam
+Type: FULL LEVEL
+Decoration Status: Up To Date
+</pre>
 </div>
+<div class="button-container">
+<input id="apiX" class="input-dis" placeholder="XV Key">
+<button class="button-dis" id="apiBtn" disabled="">Download</button></div>
+</li></ul>
   `
 };
