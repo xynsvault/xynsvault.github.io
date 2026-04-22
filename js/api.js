38532f74de045c1d5e2399fe5672204040942dd9
xynsvault <xboxynfinity@gmail.com>
2026-04-22 15:58:41 -0400

    Update api.js

--- js/api.js
+++ js/api.js
@@ -125,7 +125,22 @@ Gameplay Status: Up To Date
 <div class="button-container">
 <input class="input-dis xv-key-input" placeholder="XV Key">
 <button class="button-dis download-btn" id="apiBtn" disabled="" title="The Human Limit">Download</button></div><h style="position:relative;top:1.5%;color:#444444" class="isKeyValid">NO KEY ENTERED</h>
-</li></ul>
+</li>
+
+<li class="level-container" disabled="" title="TSUNAMI" style="">
+<h2 style="font-size:30px;">TSUNAMI</h2>
+<embed style="width:425px;height:240px;" src="https://www.youtube.com/embed/2zwUeb7c3ro"><p></p><div style="background:#3d3d3d;padding:5px;border-radius:8px;width:550px;"><h3>LEVEL INFO:</h3><p style="user-select:none;opacity:0;"></p><h>Oni WHAT are you doing yo</h><p style="user-select:none;opacity:0;"></p><pre style="position:relative;width:150px;text-align:left!important;">Creator: OniLink
+Type: SEMI-FULL LEVEL
+Decoration Status: 70%-75%
+Gameplay Status: Up To Date
+</pre>
+</div>
+<div class="button-container">
+<input class="input-dis xv-key-input" placeholder="XV Key">
+<button class="button-dis download-btn" id="apiBtn" disabled="" title="The Human Limit">Download</button></div><h style="position:relative;top:1.5%;color:#444444" class="isKeyValid">NO KEY ENTERED</h>
+</li>
+
+</ul>
   `
 
     async function ifTheresReq(xvz) {
